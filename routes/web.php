582ac02e6daa93c4eb51bd892e620ab1857f8240
kannbayashi <shinji19965@icloud.com>
2020-02-18 21:15:26 +0900

    test3

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('shoumi/add', 'shoumicontroller@add');
+Route::get('shoumi/add', 'shoumicontroller@add')->middleware('auth');
 
 Auth::routes();
 
